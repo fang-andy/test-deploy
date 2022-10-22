4579120a3104d409e10f609587c39325ab1ccbb0
fang-andy <afang1094@gmail.com>
2022-10-21 18:20:54 -0700

    test

--- server/models/messages.js
+++ server/models/messages.js
@@ -1,6 +1,6 @@
 const StreamChat = require('stream-chat').StreamChat;
 
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config({ path: '.env' });
 
 const serverClient = StreamChat.getInstance(process.env.STREAM_API_KEY, process.env.STREAM_SECRET);
 
